ce654806b906b4ef32b65edab6c23f1aa32a00e6
Victoria Bilauchuk <victoriya.bilauchuk@gmail.com>
2023-03-25 22:12:17 +0200

    updates

--- src/redux/contacts/operations.js
+++ src/redux/contacts/operations.js
@@ -1,8 +1,6 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://64145ef136020cecfda6b0f0.mockapi.io';
-
 export const fetchContacts = createAsyncThunk(
   'contacts/fetchAllContacts',
   async (_, { rejectWithValue }) => {
